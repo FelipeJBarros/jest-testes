77e0133f4c0c5b972a3195abf7aa8fcc78d30bea
Felipe Barros <48369466+FelipeJBarros@users.noreply.github.com>
2022-12-04 15:49:00 -0300

    Article List testes

--- src/__tests__/ArticleList.spec.js
+++ src/__tests__/ArticleList.spec.js
@@ -1,5 +1,20 @@
 import { cleanup, render, screen } from "@testing-library/react";
 import ArticleList from "../components/ArticleList";
+import { articles } from "../mock/mockArticles";
+
+// Mocked components
+const mockArticlePreview = jest.fn();
+const mockListPagination = jest.fn();
+
+jest.mock("../components/ArticlePreview", () => (props) => {
+  mockArticlePreview(props);
+  return <mock-ArticlePreviewComponent data-testid="article-preview" />;
+});
+
+jest.mock("../components/ListPagination", () => (props) => {
+  mockListPagination(props);
+  return <mock-ListPaginationComponent />;
+});
 
 afterEach(cleanup);
 
@@ -16,66 +31,47 @@ describe("ArticleList component", () => {
     expect(screen.getByText("No articles are here... yet.")).toBeTruthy();
   });
 
-  it("Should display a preview for each article present in the list", () => {
-    const articles = [
-      {
-        author: {
-          image:
-            "https://i.pinimg.com/originals/88/94/54/8894543f60ad09591308beb6abbb39ad.png",
-          username: "UFC - Testes",
-        },
-        body: "As coisas são mt sérias",
-        createdAt: "2022-12-04T16:04:08.100Z",
-        description: "Coisas sérias",
-        favorited: false,
-        favoritesCount: 0,
-        slug: "Artigo-serio-113034",
-        tagList: [],
-        title: "Artigo sério",
-      },
-    ];
-
+  it("Should display a ArticlePreview for each article present in the list", () => {
     render(<ArticleList articles={articles} />);
+
+    const renderedItems = screen.getAllByTestId("article-preview");
+
+    expect(renderedItems.length).toEqual(articles.length);
+  });
+
+  test("If ArticleList passes an article through properties to the ArticlePreview", () => {
+    render(<ArticleList articles={articles} />);
+
+    expect(mockArticlePreview).toHaveBeenCalledWith(
+      expect.objectContaining({
+        article: articles[0],
+      })
+    );
+    expect(mockArticlePreview).toHaveBeenCalledWith(
+      expect.objectContaining({
+        article: articles[1],
+      })
+    );
+  });
+
+  test("If ArticleList passes pagination data through properties to the ListPagination", () => {
+    const onSetPage = () => {};
+
+    render(
+      <ArticleList
+        articles={articles}
+        articlesCount={articles.length}
+        currentPage={0}
+        onSetPage={onSetPage}
+      />
+    );
+
+    expect(mockListPagination).toHaveBeenCalledWith(
+      expect.objectContaining({
+        articlesCount: articles.length,
+        currentPage: 0,
+        onSetPage,
+      })
+    );
   });
 });
-
-/*
-const articles = [
-      {
-        author: {
-          bio: null,
-          following: false,
-          image:
-            "https://i.pinimg.com/originals/88/94/54/8894543f60ad09591308beb6abbb39ad.png",
-          username: "UFC - Testes",
-        },
-        body: "As coisas são mt sérias",
-        createdAt: "2022-12-04T16:04:08.100Z",
-        description: "Coisas sérias",
-        favorited: false,
-        favoritesCount: 0,
-        slug: "Artigo-serio-113034",
-        tagList: [],
-        title: "Artigo sério",
-        updatedAt: "2022-12-04T16:04:08.100Z",
-      },
-      {
-        author: {
-          bio: null,
-          following: false,
-          image:
-            "https://i.pinimg.com/originals/88/94/54/8894543f60ad09591308beb6abbb39ad.png",
-          username: "UFC - Testes",
-        },
-        body: "Teste 4",
-        createdAt: "2022-12-03T17:58:01.344Z",
-        description: "Teste 4",
-        favorited: false,
-        favoritesCount: 0,
-        slug: "Teste-4-113034",
-        tagList: [],
-        title: "Teste 4",
-        updatedAt: "2022-12-03T17:58:01.344Z",
-      },
-    ];
-*/
---
+++ src/mock/mockArticles.js
@@ -0,0 +1,35 @@
+export const articles = [
+  {
+    author: {
+      image:
+        "https://i.pinimg.com/originals/88/94/54/8894543f60ad09591308beb6abbb39ad.png",
+      username: "UFC - Testes",
+    },
+    body: "As coisas são mt sérias",
+    createdAt: "2022-12-04T16:04:08.100Z",
+    description: "Coisas sérias",
+    favorited: false,
+    favoritesCount: 0,
+    slug: "Artigo-serio-113034",
+    tagList: [],
+    title: "Artigo sério",
+  },
+  {
+    author: {
+      bio: null,
+      following: false,
+      image:
+        "https://i.pinimg.com/originals/88/94/54/8894543f60ad09591308beb6abbb39ad.png",
+      username: "UFC - Testes",
+    },
+    body: "Teste 4",
+    createdAt: "2022-12-03T17:58:01.344Z",
+    description: "Teste 4",
+    favorited: false,
+    favoritesCount: 0,
+    slug: "Teste-4-113034",
+    tagList: [],
+    title: "Teste 4",
+    updatedAt: "2022-12-03T17:58:01.344Z",
+  },
+];
