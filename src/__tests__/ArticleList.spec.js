88d0b62f1c3092b80959fe10e7457ebe9e6994a7
Felipe Barros <48369466+FelipeJBarros@users.noreply.github.com>
2022-12-04 16:20:03 -0300

    Ajustes de comentários

--- src/__tests__/ArticleList.spec.js
+++ src/__tests__/ArticleList.spec.js
@@ -6,11 +6,13 @@ import { articles } from "../mock/mockArticles";
 const mockArticlePreview = jest.fn();
 const mockListPagination = jest.fn();
 
+// Fazendo um mock do componente filho para a verificação da passagem de props.
 jest.mock("../components/ArticlePreview", () => (props) => {
   mockArticlePreview(props);
   return <mock-ArticlePreviewComponent data-testid="article-preview" />;
 });
 
+// Fazendo um mock do componente filho para a verificação da passagem de props.
 jest.mock("../components/ListPagination", () => (props) => {
   mockListPagination(props);
   return <mock-ListPaginationComponent />;
@@ -18,30 +20,43 @@ jest.mock("../components/ListPagination", () => (props) => {
 
 afterEach(cleanup);
 
+// Criando um 'contexto' de testes, todos os testes realizados estão relacionados com o componente ArticleList.
 describe("ArticleList component", () => {
+  // Caso de teste 01: Recebendo uma lista de artigos inválida/não recebida ainda.
   it("Should display a loading warning if the component receives an invalid article list", () => {
+    // Quando renderizar Articlelist, passando um lista de artigos inválida/não recebida ...
     render(<ArticleList articles={undefined} />);
 
+    // ... espero que exista um aviso em tela que informe o loading.
     expect(screen.getByText("Loading...")).toBeTruthy();
   });
 
+  // Caso de testes 02: Recebendo uma lista de artigos vazias
   it("Should display a warning if the list of articles is empty", () => {
+    // Quando renderizar Articlelist, passando um lista de artigos vazia ...
     render(<ArticleList articles={[]} />);
 
+    // ... espero que exista um aviso em tela que informe que ainda não existem artigos publicados.
     expect(screen.getByText("No articles are here... yet.")).toBeTruthy();
   });
 
+  // Caso de teste 03: Renderizando um ArticlePreview para cada item na lista de artigos.
   it("Should display a ArticlePreview for each article present in the list", () => {
+    // Quando renderizar Articlelist, passando um lista de artigos válida e não vazia ...
     render(<ArticleList articles={articles} />);
 
     const renderedItems = screen.getAllByTestId("article-preview");
 
+    // ... espero que o núemero de ArticlePreview renderizados em tela seja igual ao de artigos presentes na lista de artigos
     expect(renderedItems.length).toEqual(articles.length);
   });
 
+  // Caso de teste 04: Passando corretamente as propriedades para o componetente filho <ArticlePreview />
   test("If ArticleList passes an article through properties to the ArticlePreview", () => {
+    // Quando renderizar Articlelist, passando um lista de artigos válida e não vazia ...
     render(<ArticleList articles={articles} />);
 
+    // ... espero que cada ArticlePreview receba as propriedades correspontes aos itens da lista.
     expect(mockArticlePreview).toHaveBeenCalledWith(
       expect.objectContaining({
         article: articles[0],
@@ -54,9 +69,11 @@ describe("ArticleList component", () => {
     );
   });
 
+  // Caso de teste 04: Passando corretamente as propriedades para o componetente filho <ListPagination />
   test("If ArticleList passes pagination data through properties to the ListPagination", () => {
     const onSetPage = () => {};
 
+    // Quando renderizar Articlelist, passando um lista de artigos válida e não vazia e suas informações de paginação ...
     render(
       <ArticleList
         articles={articles}
@@ -66,6 +83,7 @@ describe("ArticleList component", () => {
       />
     );
 
+    // ... espero que o ListPagination receba as propriedades corretamente.
     expect(mockListPagination).toHaveBeenCalledWith(
       expect.objectContaining({
         articlesCount: articles.length,
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -3,20 +3,25 @@ import ListPagination from "./ListPagination";
 import React from "react";
 
 const ArticleList = (props) => {
+  // Retorna um aviso de loading caso a lista ainda não foi recebida.
   if (!props.articles) {
     return <div className="article-preview">Loading...</div>;
   }
 
+  // Se recebe uma lista vazia, exibe uma mensagem de que ainda não existem artigos
   if (props.articles.length === 0) {
     return <div className="article-preview">No articles are here... yet.</div>;
   }
 
+  // Caso receba uma lista com artigos válidos...
   return (
     <div>
+      {/* ... para cada artigo na lista renderiza um componente filho <ArticlePreview /> ... */}
       {props.articles.map((article) => {
         return <ArticlePreview article={article} key={article.slug} />;
       })}
 
+      {/* ... E passa as informações a respeito da paginação dos artigos para o componente <ListPagination /> */}
       <ListPagination
         articlesCount={props.articlesCount}
         currentPage={props.currentPage}
@@ -27,10 +32,3 @@ const ArticleList = (props) => {
 };
 
 export default ArticleList;
-
-/*
-1 - Deve exibir um aviso de loading caso ainda não tenha recebido a lista de artigos
-2 - Deve exibir um aviso caso a lista de artigos esteja vazia
-3 - Deve exibir um preview para cada artigo presente na lista
-4 - 
-*/
